9465ed6cc5e964bf774f01f3249823d9010ce6a4
desktop <desktop@gmail.com>
2020-05-29 22:00:33 -0500

    se modifico validar.php

--- admin/validar.php
+++ admin/validar.php
@@ -1,8 +1,14 @@
 <?php
-    if($_POST['nombre']=="admin" && $_POST['password']=="123"){
-        echo "bienvenido";
+    session_start();
+    if(!isset($_SESSION['administrador'])){
+        if($_POST['nombre']=="admin" && $_POST['password']=="123"){
+            $_SESSION['administrador']=$_POST['nombre'];
+        }
+    }
+    
+    if(isset($_SESSION['administrador'])){
+        echo "hola ".$_SESSION['administrador']." bienvenido";
     }else{
         header('location: index.html');
     }
-
 ?>
